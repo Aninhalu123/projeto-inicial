44e81c994162d62a1cdecb429f9877c632f811f8
vempettearmy@gmail.com <vempettearmy@gmail.com>
2024-03-18 08:26:29 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
-var botao = document.getElementById("botao")
+const botao = document.getElementById("botao");
 botao.addEventListener("click",alertar);
 
+var nome  = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("telefone");
+var email = document.getElementById("email");
+var logradouro = document.getElementById("cep");
+var numero   = document.getElementById("cep");
+var complemento = document.getElementById("cep");
+var bairro = document.getElementById("cep");
+var estado = document.getElementById("cep");
+var cidade   = document.getElementById("cep");
+
+
+
 function alertar (){
-    alert("O botao foi clicado!!!");
+    //alert("O  nome inserido foi + nome");
+
+    saida.innerText = "nome:" + nome.Value;
+    "\n E-mail: " + email.value;
 }
 
